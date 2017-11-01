e3f170395d88aedfac5b1987960ec08dbd4243a8
nikolas08 <nikolas.hava@outlook.com>
2017-11-02 00:39:05 +0100

    Add project files.

---
+++ Stack/Element.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Element<E>
+    {
+        public E desc { get; set; } //the element description
+        public Element<E> prev; //previously stacked element
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Stack<E>
+    {
+        private Element<E> top;
+        private Element<E> temp;
+
+        public E Peek() //look at the top element
+        {
+            if (top == null)
+            {
+                return default(E);
+            }
+            else return top.desc; 
+        }
+
+        public void Push(E element) //push current element to the top of the stack
+        {
+            if (top == null)
+            {
+                top = new Element<E> { desc = element, prev = null };
+            }
+            else
+            {
+                temp = new Element<E> { desc = element, prev = top };
+                top = temp;
+            }
+        }
+
+        public E Pop() //remove the top element of the stack
+        {
+            if (top == null)
+            {
+                Console.WriteLine("No elements on Stack to pop");
+                throw new NullReferenceException();
+            }
+            else
+            {
+                E temp = top.desc;
+                top = top.prev;
+                return temp;
+            }
+        }
+    }
+}
